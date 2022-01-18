7a230bbb5442b5256aa0c6158abde4cd04e4dc6f
crysosancher <vibhupandey.vp@gmail.com>
2022-01-18 13:29:15 +0530

    added diplay function inOrder

--- DSA/Tree/bt.cpp
+++ DSA/Tree/bt.cpp
@@ -9,10 +9,18 @@ struct Node{
 		left=right=NULL;
 	}
 };
+void inOrder(Node *root){
+	if(root==NULL)
+		return;
+	inOrder(root->left);
+	cout<<root->key<<" ";
+	inOrder(root->right);
+}
 int main(){
 	Node *raw=new Node(10);
 	raw->left=new Node(20);
 	raw->right=new Node(30);
-	raw->left->left=new Node(40);
-	cout<<raw;
+	raw->right->left=new Node(40);
+	raw->right->right=new Node(50);
+	inOrder(raw);
 }
