db960e0673415f4dea2dcfd78a2a2085edf709a9
sanjeevkumarray <sanjeevkumarray007@gmail.com>
2022-10-17 00:50:49 +0530

    some thing change  commit

--- string/concat.cpp
+++ string/concat.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     char s1[20] = "Moring   ";
-    char s2[30] = "evening";
+    char s2[30] = "night";
 
     strcat(s1, s2);
     cout << s1 << endl;
     return 0;
-}
+}git add .
